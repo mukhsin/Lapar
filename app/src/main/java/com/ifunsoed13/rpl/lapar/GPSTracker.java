a6f125e73717850f6b8dbdb4eed50b4141308a80
Mukhamad Mukhsin Wibowo <mmwibowo2@gmail.com>
2015-11-05 23:15:37 +0700

    Map

---
+++ app/src/main/java/com/ifunsoed13/rpl/lapar/GPSTracker.java
@@ -0,0 +1,112 @@
+package com.ifunsoed13.rpl.lapar;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class GPSTracker extends Service implements LocationListener {
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60;
+
+    protected LocationManager locationManager;
+    protected Location location;
+
+    protected boolean canGetLocation = false;
+    protected double latitude;
+    protected double longitude;
+
+    protected Context mContext;
+
+    public GPSTracker(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            this.canGetLocation = true;
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    GPSTracker.this);
+
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            }
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    GPSTracker.this);
+
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return location;
+
+    }
+
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location arg0) {
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+}
--- app/src/main/java/com/ifunsoed13/rpl/lapar/MapActivity.java
+++ app/src/main/java/com/ifunsoed13/rpl/lapar/MapActivity.java
@@ -7,16 +7,38 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class MapActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final String TAG = MapActivity.class.getSimpleName();
+
+    protected GPSTracker gps;
+    protected double latitude;
+    protected double longitude;
+    protected LatLng position;
+    protected CameraPosition cameraPosition;
+    protected MarkerOptions marker;
     protected ProgressDialog loading;
 
     @Override
@@ -25,6 +47,56 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+        gps = new GPSTracker(getApplicationContext());
+        latitude = gps.getLatitude();
+        longitude = gps.getLongitude();
+
+        ParseObject location = new ParseObject("Locations");
+        location.put("latitude", latitude);
+        location.put("longitude", longitude);
+        location.saveInBackground();
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        position = new LatLng(gps.getLatitude(), gps.getLongitude());
+        cameraPosition = new CameraPosition.Builder().target(position).zoom(5).build();
+        marker = new MarkerOptions().position(position);
+        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker));
+
+        map.addMarker(marker);
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        ParseQuery<ParseObject> query = new ParseQuery<>("Locations");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> locations, ParseException e) {
+                // Success
+                if (e == null) {
+                    double[] lat = new double[locations.size()];
+                    double[] lng = new double[locations.size()];
+
+                    for (int i = 0; i < lat.length; i++) {
+                        lat[i] = locations.get(i).getDouble("latitude");
+                        lng[i] = locations.get(i).getDouble("longitude");
+
+                        MarkerOptions newMarker = new MarkerOptions().position(new LatLng(lat[i],
+                                lng[i]));
+                        newMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker));
+
+                        map.addMarker(newMarker);
+                    }
+                } else {
+                    // Error
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+
+        });
     }
 
     @Override
